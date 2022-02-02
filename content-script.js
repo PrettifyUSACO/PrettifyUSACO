2fae4c6fabe332d40b150ee1a6c8bf3a88c508e3
Kush Khanna <57548935+krash3125@users.noreply.github.com>
2022-02-01 17:59:49 -0800

    Initial commit

---
+++ content-script.js
@@ -0,0 +1,22 @@
+console.log('hello');
+
+console.log(document.getElementsByTagName('pre'));
+
+// Array.from(document.getElementsByTagName('pre')).forEach((el) => {
+//   const copy = document.createElement('button');
+//   copy.className = 'copy-button';
+//   copy.innerHTML = 'copy';
+//   el.appendChild(copy);
+// });
+
+// const script = document.createElement('script');
+// script.src =
+//   '//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/highlight.min.js';
+
+// const link = document.createElement('link');
+// link.rel = 'stylesheet';
+// link.href =
+//   '//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/default.min.css';
+
+// document.getElementsByTagName('head')[0].appendChild(script);
+// document.getElementsByTagName('head')[0].appendChild(link);
